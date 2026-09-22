4563edfae1c0f15d4641307064a636d9128bb8b5
@itzzfalcon52 <hussaintaherkagalwala5253@gmail.com>
2026-09-22 17:23:04 +0530

    fix:improved folder structure

--- backend/app/api/routes/availability.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from ...services.data_availability_service import data_availability_service
-
-router = APIRouter()
-
-@router.get("/availability")
-def get_availability(date: str):
-    return data_availability_service.check_availability(date)
